06fff493b2479d505aac476f6a73fd89049c8382
KeithLin7024 <keithlin724@gmail.com>
2023-10-12 15:19:07 +0800

    lab02 commit

---
+++ lab02/lab02.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int64
+
+	fmt.Print("Enter a number: ")
+	fmt.Scanln(&n)
+
+	result := Sum(n)
+	fmt.Println(result)
+}
+
+func Sum(n int64) string {
+	// TODO: Finish this function
+	ans := 0
+	str := ""
+	last := n
+
+	if n%7 == 0 {
+		last--
+	}
+
+	for i := 1; i < int(last); i++ {
+		if i%7 == 0 {
+			continue
+		}
+		str += fmt.Sprintf("%d+", i)
+		ans += i
+	}
+
+	return fmt.Sprintf("%s%d=%d", str, last, ans+int(last))
+}
